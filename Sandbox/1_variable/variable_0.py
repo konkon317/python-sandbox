fa0bc0135174ffceffcc04b83fbfdc694e07e735
KousukeFujii_win8.1_LapTop <konkon.317@gmail.com>
2018-03-06 05:00:57 +0900

    追加　変数の使い方　演算子の使い方　の練習をしたスクリプト
    branch : sub

---
+++ Sandbox/1_variable/variable_0.py
@@ -0,0 +1,61 @@
+﻿
+#コメントを日本語込みにするには utf8で保存すること
+
+print('x=1')
+x =1
+print(x)
+print()
+
+print('x=\'abc\'')
+x='abc'
+print(x)
+print()
+
+print('x=1+1')
+x=1+1
+print(x)
+print()
+
+print('x= (x==2)')
+x= (x==2)
+print(x)
+print()
+
+print('x= (x is 2)or(x is 1)')
+x= (x is 2)or(x is 1)
+print(x)
+print()
+
+print('x=True')
+x=True
+print('x= (x == 2)or(x == 1)')
+x= (x == 2)or(x == 1)
+print(x)
+print()
+
+print('x=2')
+x=2
+print('x= (x==2)and(x==1)')
+x= (x==2)and(x==1)
+print(x)
+print()
+
+print('x= 2**3')
+x=2**3
+print(x)
+print()
+
+print('x=2<<4')
+x=2<<4
+print(x)
+
+# True and True  論理積
+# True or False 論理和
+# a== 0 一致
+# a!= 0 不一致
+# is 厳密一致
+# is not 厳密不一致
+# & ビット積
+# | ビット和
+# ^ 排他的ビット和
+# ~ ビット反転
