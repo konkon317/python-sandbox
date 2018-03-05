353d30f6d233b9ebaa7d845a6d8899fe2ea342eb
KousukeFujii_win8.1_LapTop <konkon.317@gmail.com>
2018-03-06 06:32:25 +0900

    追加　複合文　if の練習
    branch sub

---
+++ Sandbox/2_compaund/if.py
@@ -0,0 +1,19 @@
+﻿
+x=0
+y=0
+z=0
+
+if x==0 :
+	y=33
+	print('true')
+	print(y)
+else :
+	z=2
+	print('false')
+	print(z)
+
+print ('end')
+
+#print (z) #//tureの場合は変数が作られないのでエラー
+#print (y) #//falseの場合は変数が作られないのでエラー
+#c++のようなブロックのスコープはないっぽい
