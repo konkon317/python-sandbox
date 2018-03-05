dac50d675da816c214eb4682d42096f8d008041f
KousukeFujii_win8.1_LapTop <konkon.317@gmail.com>
2018-03-06 08:26:34 +0900

    追加　for文の練習をしたスクリプト
    branch sub

---
+++ Sandbox/2_compaund/for.py
@@ -0,0 +1,17 @@
+﻿for i in range(5) : #range関数でリストを作成　c#のforeachのような感じ
+    print(i)
+
+print()
+
+for c in 'hello' :
+    print(c)
+
+print()
+
+for i in range(10,0,-2) :#逆順 間隔していくらはrangeで十分
+    print(i)
+
+print()
+
+# c++やc#の forでできるような変化の式を特殊な感じにしたい場合は素直にwhileにしようと思う
+
