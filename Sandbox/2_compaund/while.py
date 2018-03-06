0dcf20a7d48ed83bcb336f94881795c5cae1e19f
KousukeFujii_win8.1_LapTop <konkon.317@gmail.com>
2018-03-06 23:46:01 +0900

    追加　while文　練習スクリプト
    branch sub

---
+++ Sandbox/2_compaund/while.py
@@ -0,0 +1,18 @@
+﻿
+i=0
+
+while i<10 :
+ print(i)
+ i+=1
+
+print()
+
+i=0
+
+
+while True : # do- while　は無いっぽいので　while trueとbreakの組み合わせで
+ print(i)
+ i -=1
+
+ if not( (i < 10) and(i>-10) ):
+  break
